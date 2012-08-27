6af828b3f541adf0c42e73d76d7998bc21a072cb
Nikolay Zakharov <nikolay@desh.su>
2012-08-27 21:43:28 +0400

    Use FilterSpec instead of GenericSpec in sample_proj

--- sample_proj/polls/filters.py
+++ sample_proj/polls/filters.py
@@ -1,11 +1,12 @@
 from datafilters.filterform import FilterForm
-from datafilters.specs import (GenericSpec, DateFieldFilterSpec,
+from datafilters.filterspec import FilterSpec
+from datafilters.specs import (DateFieldFilterSpec,
     GreaterThanFilterSpec, ContainsFilterSpec,
     GreaterThanZeroFilterSpec)
 
 
 class PollsFilterForm(FilterForm):
-    has_exact_votes = GenericSpec('choice__votes')
+    has_exact_votes = FilterSpec('choice__votes')
     has_choice_with_votes = GreaterThanZeroFilterSpec('choice__votes')
     pub_date = DateFieldFilterSpec('pub_date', label='Date of publishing')
     has_major_choice = GreaterThanFilterSpec('choice__votes', value=50)
