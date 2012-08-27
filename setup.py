46c1c7ec0bf0156114ffef25cc8c1bbc8a7c26bc
Nikolay Zakharov <nikolay@desh.su>
2012-08-27 22:07:20 +0400

    Version update: 0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme_file = 'README.rst'
 
 setup(
     name='datafilters',
-    version='0.2.1',
+    version='0.3.1',
     packages=find_packages('.'),
     package_data = {'': [
         'locale/*/LC_MESSAGES/django.po',
